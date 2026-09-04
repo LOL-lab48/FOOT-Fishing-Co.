6309db238855c6c352cf9fa40e1ede1f67f25220
LOL-lab48 <236125732+LOL-lab48@users.noreply.github.com>
2026-09-05 08:35:52 +1000

    Update fishing rod details in products.js

--- products.js
+++ products.js
@@ -1,30 +1,37 @@
 const PRODUCTS = [
   {
     id: "rod1",
-    name: "Ocean Starter Rod",
-    description: "Perfect beginner rod for calm ocean and beach fishing.",
-    price: 79,
+    name: "Shimano Raider",
+    description: "Best for: Everyday anglers and general spinning.Features: Versatile actions and durable graphite builds.Why buy: It offers reliable quality without a high price tag",
+    price: 139,
     category: "Beginner"
   },
   {
     id: "rod2",
-    name: "Harbour Teen Rod",
-    description: "Lightweight rod designed for younger anglers and teens.",
-    price: 129,
+    name: "Daiwa TD Battler AGS ",
+    description: "The  Daiwa TD Battler AGS features air guide system (AGS) carbon guides, X45X technology, and Zero reel seats for ultimate sensitivity and weight reduction.",
+    price: 750,
     category: "Teen"
   },
   {
     id: "rod3",
-    name: "Deep Sea Cast Pro",
-    description: "Stronger rod built for bigger fish and rough conditions.",
-    price: 199,
+    name: "Daiwa 24 Aird X Rod",
+    description: "Amazing cheap rod. Priced around $60–$70, this lightweight graphite carbon rod offers great sensitivity, Braiding-X blank technology, and Fuji Corrosion Control guides. It is ideal for general estuary and freshwater spinning",
+    price: 65,
     category: "Teen"
   },
   {
     id: "rod4",
-    name: "FOOT All-Rounder Rod",
-    description: "Balanced rod for beginners and casual anglers.",
-    price: 149,
+    name: "Shimano Zodias",
+    description: "Best for: Light tackle, estuary, and bass fishing.Features: Hi-Power X construction and CI4+ reel seat.Why buy: It provides high-end performance at a lower price point.",
+    price: 350,
     category: "Beginner"
+  },
+  {
+    id: "rod5",
+    name: "Shimano Poison Adrena",
+    description: "Best for: Freshwater lure and bass fishing.Features: Full carbon monocoque handle and Spiral X Core construction.Why buy: It delivers maximum bite feedback and incredible casting accuracy.",
+    price: 580,
+    category: "Teen"
   }
 ];
